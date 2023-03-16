3a98f450391df704275b1cd27bc9c1fe97d3ccda
kshields412 <kshields412@gmail.com>
2023-03-15 22:30:45 -0400

    setting up structure

---
+++ service/encryption.py
@@ -0,0 +1,11 @@
+
+def encrypt():
+    # translate english to morse code
+    return None
+
+
+def decrypt():
+    # translate morse code to English
+    return None
+
+
---
+++ service/translation.py
@@ -0,0 +1,11 @@
+def detect_language():
+    return None
+
+
+def translate_to_english():
+    return None
+
+
+def translate_to(language: str):
+    # make language an enum. translate morse code > english > selected language
+    return None
